7e0b5f4841d1dce2dad13183e4f08aa9dce57acc
skywalkerkabza <157689162+skywalkerkabza@users.noreply.github.com>
2024-01-30 11:22:49 +0200

    Added negative test case and error handling strategies

--- __tests__/ExchangeRateAPI/ExchangeRateAPI.jest.js
+++ __tests__/ExchangeRateAPI/ExchangeRateAPI.jest.js
@@ -8,39 +8,58 @@ const instance = axios.create({
     })
 });
 
+
 describe('Exchange Rate API Tests', () => {
-    test('get latest exchange', async () => {
-        try {
-            const response = await instance.get('https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD');
+    
+  test('Verify the HTTP status is 200', async () => {
+      try {
+          const response = await instance.get('https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD');
+           
+           console.log('Response Data:', response.data); // Print out response.data
+          expect(response.status).toBe(200);
+      } catch (error) {
+          console.error('Error:', error.message);
+          expect(error).toBeNull();
+      }
+  });
 
-            // Print out response.data
-            console.log('Response Data:', response.data);
+  test('Count the total number of currencies returned within the response', async () => {
+      try {
+          const response = await instance.get('https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD');
+          const responseData = response.data;
+          expect(responseData).toHaveProperty('conversion_rates');
+          const conversionRates = responseData.conversion_rates;
+          console.log('Conversion Rates:', conversionRates);// Print Conversion Rates
+          const currencyCount = Object.keys(conversionRates).length;
+          console.log('Number of Currencies:', currencyCount);// Print Count the total number of currencies returned within the response
+          expect(currencyCount).toBeGreaterThan(0);
+      } catch (error) {
+          console.error('Error:', error.message);
+          expect(error).toBeNull();
+      }
+  });
 
-            
-            expect(response.status).toBe(200);// test to check 200
+  test('Verify the currency "GBP" is shown within the response', async () => {
+      try {
+          const response = await instance.get('https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD');
+          const responseData = response.data;
+          const conversionRates = responseData.conversion_rates;
+          expect(conversionRates).toHaveProperty('GBP');
+      } catch (error) {
+          console.error('Error:', error.message);
+          expect(error).toBeNull();
+      }
+  });
 
-            
-            const responseData = response.data;
-            expect(responseData).toHaveProperty('conversion_rates');
-            const conversionRates = responseData.conversion_rates;
-
-            
-            console.log('Conversion Rates:', conversionRates);// Print Conversion Rates
-
-            const currencyCount = Object.keys(conversionRates).length;
-            
-            console.log('Number of Currencies:', currencyCount);// Print Count the total number of currencies returned within the response
-
-
-            expect(currencyCount).toBeGreaterThan(0);
-
-            
-            expect(conversionRates).toHaveProperty('GBP');// Verify the currency 'GBP' is shown within the response
-        } catch (error) {
-            
-            console.error('Error:', error.message);// Handle errors
-            
-            expect(error).toBeNull();
-        }
-    });
+  test('Verify that when an invalid currency "ZA" that the test fails and prints out error Stack ', async () => {
+    try {
+        const response = await instance.get('https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD');
+        const responseData = response.data;
+        const conversionRates = responseData.conversion_rates;
+        expect(conversionRates).toHaveProperty('ZA');
+    } catch (error) {
+        console.error('Error:', error.message);
+        expect(error);// we expecting an error
+    }
+});
 });
